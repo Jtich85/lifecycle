60b116c849cb0907a1d365485ce192fef5ac2b4d
PSM\javiert <javiert@powerstarmex.com>
2024-09-10 14:14:13 -0600

    final de la seccion 20

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrl: './price.component.css',
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy{
+
+  @Input()
+  public price: number = 0;
+
+  public intervarl$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Componente Hijo: ngOnInit');
+
+    this.intervarl$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`) );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente Hijo: ngOnChanges');
+    console.log({changes});
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente Hijo: ngOnDestroy');
+    this.intervarl$?.unsubscribe();
+  }
+
+}
